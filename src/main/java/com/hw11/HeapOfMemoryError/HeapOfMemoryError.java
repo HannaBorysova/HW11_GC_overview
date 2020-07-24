1a035f70888ad8928bdad37c07f7b0099c8f1ac2
Анна <haborysova@gmail.com>
2020-07-24 15:02:38 +0300

    HW11_GC_overview

---
+++ src/main/java/com/hw11/HeapOfMemoryError/HeapOfMemoryError.java
@@ -0,0 +1,10 @@
+package main.java.com.hw11.HeapOfMemoryError;
+
+public class HeapOfMemoryError {
+    public static final int MAX_ARRAY_RANGE = 1300000000;
+
+    public static void printHeapOfMemoryError(){
+        int[] newArray = new int[MAX_ARRAY_RANGE];
+            System.out.println(newArray);
+    }
+}
---
+++ src/main/java/com/hw11/Main.java
@@ -0,0 +1,10 @@
+package main.java.com.hw11;
+
+import main.java.com.hw11.HeapOfMemoryError.HeapOfMemoryError;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HeapOfMemoryError.printHeapOfMemoryError();
+    }
+}
